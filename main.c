fd2fd93e224f634fa79069fa92818f8db8b164f6
Dani <danielpasserini@outlook.com>
2022-07-30 14:47:10 +0200

    esta carpeta es una prueba para pablito

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>/* Archivos de cabecera */
+int main()
+{
+	int nombre;/* Declaración de varialbe */
+	int nombre2;/* Declaración de variable */
+	nombre = 13;
+	nombre2 = 22;
+
+	printf("hello world\n");/* Sentencias */ 
+	printf ("Hello %d dani\n %d\n", nombre, nombre2);/* Sentencias */
+}
+
